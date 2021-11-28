76024ab9109de74c03f44ae0aed9a5feacae6aa5
Christian Fei <crifei93@gmail.com>
2021-11-28 17:13:57 +0100

    layout and ui

--- lib/rssix_web/live/entry_live/show.ex
+++ lib/rssix_web/live/entry_live/show.ex
@@ -15,9 +15,21 @@ defmodule RssixWeb.EntryLive.Show do
     {:noreply,
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:entry, Entries.get_entry!(id))}
+     |> assign(:entry, Entries.get_entry!(id))
+     |> assign(:entries, last_10_unread_entries())}
+  end
+
+  @impl true
+  def handle_event("read", %{"id" => id}, socket) do
+    {:ok, _} = Rssix.Entries.read_entry(id)
+
+    {:noreply, assign(socket, :entries, last_10_unread_entries())}
   end
 
   defp page_title(:show), do: "Show Entry"
   defp page_title(:edit), do: "Edit Entry"
+
+  defp last_10_unread_entries do
+    Rssix.Entries.last_10_unread_entries()
+  end
 end
